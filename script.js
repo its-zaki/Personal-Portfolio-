bcfed94517a84c42848647c8d77021f8824684f9
its zaki <zakiyounus789@gmail.com>
2023-12-17 00:45:06 +0500

    personal porfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+// Get tabLinks & tabContents
+var tabLinks = document.getElementsByClassName("tab-links"),
+  tabContents = document.getElementsByClassName("tab-contents");
+
+// On openTab fucntion call
+function openTab(tabname) {
+  for (item of tabLinks) {
+    item.classList.remove("active-link");
+  }
+  for (item of tabContents) {
+    item.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Get SideMenu
+var sideMenu = document.getElementById("sidemenu");
+
+// On openTab fucntion call
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
+
+
+var typed = new Typed(".profession", {
+  strings: ["frontend Developer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
